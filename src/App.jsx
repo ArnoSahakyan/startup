d5ce578cf14ed6f58078d9a0c88ddf298c32e61c
Rubo001 <ruben.budaghyan@icloud.com>
2024-04-06 18:12:35 +0400

    Arno

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.scss'
-
+import Header from './components/Header/Header'
 function App() {
-
   return (
     <div className="App">
-      <h1>Dev Branch</h1>
+    <Header />
     </div>
   )
 }
