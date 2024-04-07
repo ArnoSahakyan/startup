d42a36045bbb2a9404afc4c60808bd06178ce2a4
ArnoSahakyan <sahakyan.arno@gmail.com>
2024-04-07 21:45:10 +0400

    features done, maybe needs fixes later

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 import Info from './components/Info/Info'
 import Header from './components/Header/Header'
+import Features from './components/Features/Features'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Info />
+      <Features />
     </div>
   )
 }
