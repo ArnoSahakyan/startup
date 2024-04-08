9f9032eee72196c4d781f12dd3eabe8eeb2f3b68
Rubo001 <ruben.budaghyan@icloud.com>
2024-04-08 22:41:36 +0400

    Arnulikkkk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 import Info from './components/Info/Info'
 import Header from './components/Header/Header'
+import InputArea from './components/InputArea/InputArea'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Info />
+      <InputArea />
     </div>
   )
 }
